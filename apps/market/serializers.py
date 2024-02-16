dc5f19715c5cb2c8c9c648a95eb548c52e2b2bce
PavelNarkevich <f1tzz@mail.ru>
2024-02-16 12:15:08 +0300

    Creating app "Catalog Car" and add Model in db

---
+++ apps/market/serializers.py
@@ -0,0 +1,69 @@
+
+from rest_framework import serializers
+
+from apps.market.models import (
+    Company,
+    WorkTime,
+    SocialNetwork,
+    News,
+)
+
+
+class WorkTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkTime
+        fields = ['day', 'time']
+
+
+class SocialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialNetwork
+        fields = ['url', 'name']
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    work_time = WorkTimeSerializer(many=True, read_only=True)
+    social_network = SocialSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Company
+        fields = [
+            'id',
+            'about_company',
+            'address',
+            'work_phone',
+            'social_network',
+            'work_time',
+            'logo',
+        ]
+
+
+class UpdateCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = [
+            'id',
+            'about_company',
+            'address',
+            'work_phone',
+            'social_network',
+            'work_time',
+            'logo',
+        ]
+
+
+class NewsSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
+class CreateNewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = [
+            'title',
+            'content',
+        ]

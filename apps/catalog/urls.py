f4c041ee270195bc1253a9d863ba336337520501
PavelNarkevich <f1tzz@mail.ru>
2024-03-01 12:50:39 +0300

    update CatalogCarSerializer in fields add 'photo'

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -27,7 +27,7 @@ from apps.catalog.views import (
 
 urlpatterns = [
     path('', GetCatalogCarsGenericView.as_view()),
-    path(' <str:body>/', GetChoiceTypeCarGenericView.as_view()),
+    path('bmw/<str:body>/', GetChoiceTypeCarGenericView.as_view()),
     path('create_cars/', CreateCarsGenericView.as_view()),
     path('create_cars/<int:car_id>/', UpdateDeleteCarsGenericView.as_view()),
     path('create_el/', CreateCarElementGenericView.as_view()),
@@ -36,10 +36,10 @@ urlpatterns = [
     path('create_component/<int:id_component>/', UpdateCarComponentGenericView.as_view()),
     path('create_detail/', CreateDetailsGenericView.as_view()),
     path('create_detail/<int:id_detail>/', UpdateDetailsGenericView.as_view()),
-    path(' <str:body>/<int:car_id>/', GetCarGenericView.as_view()),
-    path(' <str:body>/<int:car_id>/<int:elem_id>/', GetComponentGenericView.as_view()),
-    path(' <str:body>/<int:car_id>/<int:elem_id>/<int:component_id>/', GetDetailsGenericView.as_view()),
-    path(' <str:body>/<int:car_id>/<int:elem_id>/<int:component_id>/addcart/', AddBasketGenericView.as_view()),
+    path('bmw/<str:body>/<int:car_id>/', GetCarGenericView.as_view()),
+    path('bmw/<str:body>/<int:car_id>/<int:elem_id>/', GetComponentGenericView.as_view()),
+    path('bmw/<str:body>/<int:car_id>/<int:elem_id>/<int:component_id>/', GetDetailsGenericView.as_view()),
+    path('bmw/<str:body>/<int:car_id>/<int:elem_id>/<int:component_id>/addcart/', AddBasketGenericView.as_view()),
     path('test/', TestGenericView.as_view()),
     path('cart/', GetBasketGenericView.as_view()),
     path('cart/<int:id_detail>/', UpdateDeletedBasketGenericView.as_view()),
